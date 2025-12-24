a88706ad43317a26af3a53ba97e404668a58d3c5
danindu2024 <daninduransika@gmail.com>
2025-12-24 23:01:21 +0530

    feat: add board routes to the server

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import authRoutes from './routes/authRoutes';
+import boardRoutes from './routes/boardRoutes';
 import { errorHandler } from './middleware/errorHandler';
 
 const app = express();
@@ -11,13 +12,14 @@ app.use(cors());
 
 // Routes
 app.use('/api/auth', authRoutes);
+app.use('/api/boards', boardRoutes);
 
 // Basic route
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
-// Error handler (must be last)
+// Error handler
 app.use(errorHandler);
 
 export default app;
