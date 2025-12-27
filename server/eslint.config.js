2375aac4110f3a8e232c04e398e4714369b8f0c1
danindu2024 <daninduransika@gmail.com>
2025-12-27 07:19:35 +0530

    style: add typescript eslint for formatting

---
+++ server/eslint.config.js
@@ -0,0 +1,18 @@
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+
+export default [
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: tsParser,
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-explicit-any": "warn"
+    },
+  },
+];
