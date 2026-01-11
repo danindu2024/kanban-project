5e0f5676353d8c4bd540194e7283f24fb5c87591
danindu2024 <daninduransika@gmail.com>
2026-01-12 01:23:03 +0530

    fix: pass userRepository to column controller

--- server/src/routes/columnRoutes.ts
+++ server/src/routes/columnRoutes.ts
@@ -1,12 +1,15 @@
 import { ColumnController } from "../controllers/columnController";
 import { ColumnRepository } from "../infrastructure/repositories/ColumnRepository";
 import { BoardRepository } from "../infrastructure/repositories/BoardRepository";
+import { UserRepository } from "../infrastructure/repositories/UserRepository";
 import { Router } from "express";
 import {protect} from '../middleware/authMiddleware'
 
 const columnRepository = new ColumnRepository()
 const boardRepository = new BoardRepository()
-const columnController = new ColumnController(columnRepository, boardRepository)
+const userRepository = new UserRepository()
+
+const columnController = new ColumnController(columnRepository, boardRepository, userRepository)
 
 const router = Router()
 
