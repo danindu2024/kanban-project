639fb59925cd3a5726b6e5dc6cca449caa441d72
danindu2024 <daninduransika@gmail.com>
2026-01-18 23:36:05 +0530

    fix: remove manually passing order. Now It handled by repository. Refactor to improve code readability

--- server/src/domain/repositories/IColumnRepository.ts
+++ server/src/domain/repositories/IColumnRepository.ts
@@ -1,14 +1,18 @@
 import { Column } from '../entities/Column';
 
 export interface IColumnRepository {
+  // create column
   create(columnData: 
-    { board_id: string; 
+    { 
+      board_id: string; 
       title: string; 
-      order: number;}): Promise<Column>;
+    }
+  ): Promise<Column>;
 
   findByBoardId(boardId: string): Promise<Column[]>;
   update(columnId: string, title: string): Promise<Column | null>;
   findById(id: string): Promise<Column | null>;
   delete(id: string): Promise<Boolean>;
   moveColumn(id: string, newOrder: number): Promise<void>
+  countColumn(boradId: string): Promise<number>
 }
