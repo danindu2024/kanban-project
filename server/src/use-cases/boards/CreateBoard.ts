f7b0359f6d0303ff8f4c43a1ccd9213152e9eca5
danindu2024 <daninduransika@gmail.com>
2026-01-19 15:32:14 +0530

    fix: update title validation logic for proper sanitize. Add comment regarding why members are not assigned in create board

--- server/src/use-cases/boards/CreateBoard.ts
+++ server/src/use-cases/boards/CreateBoard.ts
@@ -2,10 +2,13 @@ import { IBoardRepository } from "../../domain/repositories/IBoardRepository";
 import { IUserRepository } from "../../domain/repositories/IUserRepository";
 import { AppError } from "../../utils/AppError";
 import { ErrorCodes } from "../../constants/errorCodes";
+import { businessRules } from "../../constants/businessRules";
 
 interface CreateBoardRequestDTO {
   title: string;
   owner_id: string;
+  // members are not add for the board when creating. They will be added later in a separate function.
+  // maintain atomic rule
 }
 
 interface CreateBoardResponseDTO {
@@ -28,26 +31,29 @@ export class CreateBoard {
   async execute({ title, owner_id }: CreateBoardRequestDTO): Promise<CreateBoardResponseDTO> {
     // validate user exists
     const user = await this.userRepository.findById(owner_id);
-    if (!user) throw new AppError(ErrorCodes.USER_NOT_AUTHENTICATED, "User not found. Unauthorized", 404);
+    if (!user) throw new AppError(ErrorCodes.USER_NOT_FOUND, "User not found", 404);
+
+    //sanitize title
+    const sanitizedTitle = (title || "").trim()
 
     // title validation
-    if(!title){
+    if(sanitizedTitle.length === 0){
       throw new AppError(
-        ErrorCodes.MISSING_INPUT, 
+        ErrorCodes.MISSING_REQUIRED_FIELDS, 
         "Title is required to create a board.",
         400
       );
     }
-    if(title.length > 100){
+    if(sanitizedTitle.length > businessRules.MAX_BOARD_TITLE_LENGTH){
       throw new AppError(
         ErrorCodes.VALIDATION_ERROR,
-        "Board title must be less than 100 characters.",
+        `Board title must be less than ${businessRules.MAX_BOARD_TITLE_LENGTH} characters`,
         400
       );
     }
 
     const board = await this.boardRepository.create({
-      title,
+      title: sanitizedTitle,
       owner_id,
       members: [],
     });
