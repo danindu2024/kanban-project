dbf3c2ddbd90ea7e3e84271b6b8f9b7823968cb1
danindu2024 <daninduransika@gmail.com>
2026-01-19 15:33:49 +0530

    fix: add extented interface for column population. Add helper interface for task population

--- server/src/domain/entities/Board.ts
+++ server/src/domain/entities/Board.ts
@@ -1,7 +1,21 @@
+import { Column } from "./Column";
+import { Task } from "./Task";
+
 export interface Board{
   id: string;
   title: string;
   owner_id: string;
   members: string[];
   created_at: Date;
+  updated_at: Date;
+}
+
+// 2. extended interface for populated columns
+export interface PopulatedBoard extends Board {
+  columns: PopulatedColumn[]; 
+}
+
+// helper for column to task population
+export interface PopulatedColumn extends Column {
+  tasks: Task[];
 }
