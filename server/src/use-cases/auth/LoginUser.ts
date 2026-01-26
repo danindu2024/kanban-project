6735a5f238f92b03b68a683e80c6b56d91594362
danindu2024 <daninduransika@gmail.com>
2026-01-27 00:49:21 +0530

    fix: add basic input sanitation. Change 'present validation error 'from invalide credentials to missing required fields. Change email format validation error from invalide credentials to validation error

--- server/src/use-cases/auth/LoginUser.ts
+++ server/src/use-cases/auth/LoginUser.ts
@@ -27,28 +27,33 @@ export class LoginUserUseCase {
 
   async execute({ email, password }: LoginRequestDTO): Promise<LoginResponseDTO> {
 
+    // basic input sanitation
+    // remove leading and trailing whitespaces
+    const sanitizedEmail = (email || "").trim().toLowerCase()
+    const sanitizedPassword = (password || "").trim()
+
     // Basic presence validation
-    if (!email || !password) {
-      throw new AppError(ErrorCodes.INVALID_CREDENTIALS, 'Missing email or password', 401);
+    if (!sanitizedEmail || !sanitizedPassword) {
+      throw new AppError(ErrorCodes.MISSING_REQUIRED_FIELDS, 'Missing required fields', 400);
     }
 
     // Validate email format
     const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-    if (!emailRegex.test(email)) {
-      throw new AppError(ErrorCodes.INVALID_CREDENTIALS, 'Invalid email or password', 401);
+    if (!emailRegex.test(sanitizedEmail)) {
+      throw new AppError(ErrorCodes.VALIDATION_ERROR, 'Invalid email format', 400);
     }
 
     // Check if user exists
-    const user = await this.userRepository.findByEmail(email);
+    const user = await this.userRepository.findByEmail(sanitizedEmail);
     
     if (!user) {
-      throw new AppError(ErrorCodes.INVALID_CREDENTIALS, 'Invalid credentials', 401); 
+      throw new AppError(ErrorCodes.INVALID_CREDENTIALS, 'Invalide email or password', 401); 
     }
 
     // Check if password matches
-    const isMatch = await bcrypt.compare(password, user.password_hash);
+    const isMatch = await bcrypt.compare(sanitizedPassword, user.password_hash);
     if (!isMatch) {
-      throw new AppError(ErrorCodes.INVALID_CREDENTIALS, 'Invalid credentials', 401);
+      throw new AppError(ErrorCodes.INVALID_CREDENTIALS, 'Invalide email or password', 401);
     }
 
     // Generate Token
