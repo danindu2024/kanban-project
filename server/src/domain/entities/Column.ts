5c85c71495d3a1c3919cdedc7a9918f53d48fca1
danindu2024 <daninduransika@gmail.com>
2026-01-26 23:49:32 +0530

    fix: remove tasks property from column. Now it populated by board entity

--- server/src/domain/entities/Column.ts
+++ server/src/domain/entities/Column.ts
@@ -1,11 +1,8 @@
-import { Task } from "./Task";
-
 export interface Column {
   id: string;
   board_id: string;
   title: string;
   order: number;
-  tasks?: Task[];
   created_at: Date;
   updated_at: Date;
 }
